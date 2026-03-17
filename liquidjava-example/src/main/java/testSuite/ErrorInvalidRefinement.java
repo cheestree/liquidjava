834ff5e666d28190b4c352b6be5b5e2745eb32c7
Daniel <101189638+cheestree@users.noreply.github.com>
2026-03-17 14:33:35 +0000

    Merged invalid refinements and unary operators
    testMultiple should be refactored in another PR, as warning support isn't implemented.

--- liquidjava-example/src/main/java/testSuite/ErrorInvalidRefinement.java
+++ liquidjava-example/src/main/java/testSuite/ErrorInvalidRefinement.java
@@ -5,8 +5,15 @@ import liquidjava.specification.Refinement;
 @SuppressWarnings("unused")
 public class ErrorInvalidRefinement {
 
-    void test() {
+    void invalidRefinement1() {
         @Refinement("x") // Invalid Refinement Error
         int x = 0;
+    }
+
+    void invalidRefinement2(@Refinement("y + 1") int y) { // Invalid Refinement Error
+    }
+
+    @Refinement("_ * 2") // Invalid Refinement Error
+    void invalidRefinement3() {
     }
 }
--- liquidjava-example/src/main/java/testSuite/ErrorUnaryOperators.java
+++ liquidjava-example/src/main/java/testSuite/ErrorUnaryOperators.java
@@ -4,12 +4,18 @@ import liquidjava.specification.Refinement;
 
 @SuppressWarnings("unused")
 public class ErrorUnaryOperators {
-    public static void main(String[] args) {
+    public static void unaryOperator1() {
         @Refinement("_ < 10")
         int v = 3;
         v--;
         @Refinement("_ >= 10")
         int s = 10;
         s--; // Refinement Error
+    }
+
+    public static void unaryOperator2() {
+        @Refinement("b > 0")
+        int b = 8;
+        b = -b; // Refinement Error
     }
 }
